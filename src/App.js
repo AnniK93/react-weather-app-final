b43e522e9f53fe02a53a635ec749a0b1dc5a8327
Annika <85462906+AnniK93@users.noreply.github.com>
2022-03-11 23:33:41 +0000

    Fixed problem with background

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Search from "./Search";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header></header>
-        <main>
-          <Search />
-        </main>
-        <footer>
-          <a href="https://github.com/AnniK93/react-weather-app-final">
-            Open source code
-          </a>{" "}
-          by Anni
-        </footer>
+    <body>
+      <div className="App">
+        <div className="container">
+          <header></header>
+          <main>
+            <Search />
+          </main>
+          <footer>
+            <a href="https://github.com/AnniK93/react-weather-app-final">
+              Open source code
+            </a>{" "}
+            by Anni
+          </footer>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
